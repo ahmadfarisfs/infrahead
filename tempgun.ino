e37d6d158fe4d63d507db82273c18cab99ae30e4
ahmadfarisfs <ahmadfarisfs@gmail.com>
2020-02-07 22:29:58 +0700

    Update tempgun.ino

--- tempgun.ino
+++ tempgun.ino
@@ -1,13 +1,18 @@
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 
+#define PRE_CORR_FACTOR 0.0
+#define CORRECTION_FACTOR  -0.75
+
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
-float calculateForeheadToBody(float x) {
+float calculateForeheadToBody(float y) {
   //https://docs.google.com/spreadsheets/d/1EtJas-raX3Qd3OiARKWVPCCqhRaQXOXToTx4brYeH8Q/edit?usp=sharing
   //based on https://www.omronhealthcare-ap.com/Content/uploads/products/68363e5e293440bf95f724dceb4fa860.pdf
   //page 22
-  return (x * x) * 0.1944810311 - (x * 12.80081016) + 247.1541305;
+
+  float x = y + PRE_CORR_FACTOR;
+  return (x * x) * 0.1944810311 - (x * 12.80081016) + 247.1541305 + CORRECTION_FACTOR;
 }
 
 void setup() {
